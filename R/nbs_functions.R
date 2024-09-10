fa71e5ada49247b85dbd936fcefb981ed2c432b6
Greg Chambers <greg.chambers@tn.gov>
2024-09-10 11:06:05 -0500

    edit to note field set

--- R/nbs_functions.R
+++ R/nbs_functions.R
@@ -515,6 +515,7 @@ edit_date<-function(id, val){
 #' @export
 edit_note<-function(id, val){
   edit_text_field(id,as.character(val), id_suffix = '', id_type = 'id')
+  Sys.sleep(.5)
   oku<-unlist(remDr$findElement('id',id)$getElementAttribute('onchange'))
   button_ui<-substr(oku, str_locate(oku,"unhideBatchImg\\('")[2]+1,nchar(oku)-3)
   remDr$findElement('id',paste0('AddButtonToggle',button_ui))$findChildElement('tag name','input')$clickElement()
