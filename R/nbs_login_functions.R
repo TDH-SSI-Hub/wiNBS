a7f8a5e98905304e51ae02ea6e8c4d96ea396964
Greg Chambers <greg.chambers@tn.gov>
2024-09-25 08:08:55 -0500

    file reorganization

---
+++ R/nbs_login_functions.R
@@ -0,0 +1,62 @@
+#' Store password for an NBS account
+#'
+#' Uses the keyring package to store credentials locally.
+#'
+#' @param username Username for account
+#' @param password Password for account
+#' 
+#' @return Nothing
+#' @export
+nbs_password_set<-function(username,password=''){
+  message('Password set. Delete lines containing the password from this file.')
+  keyring::key_set_with_value('NBS',username,password)
+}
+
+
+#' Retrieve NBS password for an account
+#'
+#' Retrieves a password set with nbs_password_set.
+#'
+#' @param username NBS username
+#' 
+#' @return NBS password
+#' @export
+nbs_password_get<-function(username){
+  if(username %in% keyring::key_list('NBS')$username){
+    message('Do not store your password in a script.')
+    return(keyring::key_get('NBS',username))
+  }else{
+    message(paste0('No credentials found for ',username,'. Use nbs_password_set() to create credentials.'))
+    return(NA)
+  }
+  return(NA)
+  
+}
+
+#' Load NBS home screen
+#'
+#' Navigate to the NBS login page, enter username and password
+#' , and enter the specified environment - defaults to production
+#'
+#' @param u NBS Username
+#' @param environment Environment to enter
+#' @param url url for NBS login page - e.g., 'https://hssi.tn.gov/auth/login'
+#' @return Nothing
+#' @export
+nbs_load <- function(u = "", environment = "NBS Production", url = "https://hssi.tn.gov/auth/login") {
+  password<-nbs_password_get(u)
+  if(is.na(password)){
+    return(NA)  
+  }
+  remDr$navigate(url)
+  remDr$findElement(using = "name", value = "usr_name")$clearElement()
+  remDr$findElement(using = "name", value = "usr_name")$sendKeysToElement(list(u))
+  remDr$findElement(using = "name", value = "usr_password")$clearElement()
+  remDr$findElement(using = "name", value = "usr_password")$sendKeysToElement(list(password))
+  remDr$findElements("tag name", "button")[[2]]$clickElement()
+  if (remDr$getTitle() == "HSSI Welcome") {
+    remDr$findElements("class", "btn")[[1]]$clickElement()
+  }
+  Sys.sleep(.5)
+  remDr$findElements("link text", environment)[[1]]$clickElement()
+}
